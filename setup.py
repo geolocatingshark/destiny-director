b6059f4367331e227dea8382daf154cff835b0ef
GS <gsfernandes81@gmail.com>
2022-06-25 17:36:47 +0100

    Add requests to setup.py deps

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name="sector_accounting",
-    version="0.1.2",
+    version="0.1.3",
     description="Discord Client Multiplexor with Discord Components support",
     url="https://github.com/gs729/sector_accounting",
     author="GS",
     author_email="geolocatingshark@gmail.com",
     license="GNU GPLv3",
     packages=["sector_accounting"],
-    install_requires=["gspread", "pytz"],
+    install_requires=["gspread", "pytz", "requests>=2.28.0"],
     zip_safe=False,
 )
