b54269f34de9be73a4beb93aae14da3694014794
gs729 <gsfernandes81@gmail.com>
2023-04-13 19:04:05 +0100

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="sector_accounting",
-    version="0.1.4",
+    version="0.2.0",
     description="Discord Client Multiplexor with Discord Components support",
     url="https://github.com/gs729/sector_accounting",
     author="GS",
