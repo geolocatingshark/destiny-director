d3473f9b27dfc3dbd4f1b6240af89a1e9709962f
gsfernandes81 <gsfernandes81@gmail.com>
2024-12-03 21:07:31 -0600

    Disable the Iron Banner command
    Disable the iron banner command since bungie have started keeping it up
    for two weeks at a time and the bot cannot currently deal with this. The
    issue is if the bot assumes a two week period starting on odd weeks and
    ending on even weeks, then if Bungie starts an iron banner on an even
    week, she will only keep the command up for 1 week.
    
    The alternative that will be used is a manual (User) command definition

--- destiny-director/beacon/modules/iron_banner.py
+++ destiny-director/beacon/modules/iron_banner.py
@@ -15,12 +15,7 @@
 
 import datetime as dt
 
-import hikari as h
-import lightbulb as lb
-from hmessage import HMessage as MessagePrototype
-
 from ...common import cfg
-from ..nav import NavigatorView, NavPages
 from .autoposts import autopost_command_group, follow_control_command_maker
 
 REFERENCE_DATE = dt.datetime(2023, 12, 12, 17, tzinfo=dt.timezone.utc)
@@ -28,40 +23,7 @@ REFERENCE_DATE = dt.datetime(2023, 12, 12, 17, tzinfo=dt.timezone.utc)
 FOLLOWABLE_CHANNEL = cfg.followables["iron_banner"]
 
 
-async def on_start(event: h.StartedEvent):
-    global iron_banner_pages
-    iron_banner_pages = await NavPages.from_channel(
-        event.app,
-        FOLLOWABLE_CHANNEL,
-        history_len=1,
-        period=dt.timedelta(days=7),
-        reference_date=REFERENCE_DATE,
-        suppress_content_autoembeds=False,
-        no_data_message=MessagePrototype(content="No Iron Banner this week : ("),
-    )
-
-
-@lb.command("iron", "Iron banner infographic for when it's active")
-@lb.implements(lb.SlashCommandGroup)
-async def iron(ctx: lb.Context):
-    pass
-
-
-@iron.child
-@lb.command("banner", "Iron banner infographic for when it's active")
-@lb.implements(lb.SlashSubCommand)
-async def banner(ctx: lb.Context):
-    navigator = NavigatorView(
-        pages=iron_banner_pages,
-        allow_start_on_blank_page=True,
-    )
-    await navigator.send(ctx.interaction)
-
-
 def register(bot):
-    bot.command(iron)
-    bot.listen()(on_start)
-
     autopost_command_group.child(
         follow_control_command_maker(
             FOLLOWABLE_CHANNEL, "iron_banner", "Iron Banner", "Iron Banner auto posts"
