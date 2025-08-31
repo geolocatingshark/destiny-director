082d70eed7de5b9a1a7f0c77639bfe5c36f0f5ca
gsfernandes81 <gsfernandes81@gmail.com>
2025-08-31 08:31:43 +0200

    Remove vestigial duplicate function duplicate

--- dd/anchor/utils.py
+++ dd/anchor/utils.py
@@ -288,26 +288,3 @@ def check_admin(func):
         return await func(ctx, *args, **kwargs)
 
     return wrapper
-
-
-def ensure_session(sessionmaker):
-    """Decorator for functions that optionally want an sqlalchemy async session
-
-    Provides an async session via the `session` parameter if one is not already
-    provided via the same.
-
-    Caution: Always put below `@classmethod` and `@staticmethod`"""
-
-    def ensured_session(f: t.Coroutine):
-        async def wrapper(*args, **kwargs):
-            session = kwargs.pop("session", None)
-            if session is None:
-                async with sessionmaker() as session:
-                    async with session.begin():
-                        return await f(*args, **kwargs, session=session)
-            else:
-                return await f(*args, **kwargs, session=session)
-
-        return wrapper
-
-    return ensured_session
